3b3fa8ad1dcd630a102efec5e7e09fbc59d5acc9
Lonalt <luann.alves353@gmail.com>
2023-04-27 05:10:26 -0300

    renamed:    Hemoce/.vscode/settings.json -> HSS/.vscode/settings.json renamed:    Hemoce/README.md -> HSS/README.md renamed:    Hemoce/src/Hemoce.java -> HSS/src/Hemoce.java new file:   HSS/src/Pessoa.java deleted:    Hemoce/src/Pessoa.java

---
+++ HSS/src/Pessoa.java
@@ -0,0 +1,28 @@
+public class Pessoa {
+    String nome;
+    int idade;
+    String tipoSanguineo;
+    float peso;
+    float altura;
+    String sexo;
+    boolean saldavel;
+    boolean doador;
+
+    public Pessoa(String nome, int idade, String tipoSanguineo, float peso, float altura, String sexo, boolean saldavel, boolean doador) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipoSanguineo = tipoSanguineo;
+        this.peso = peso;
+        this.altura = altura;
+        this.sexo = sexo;
+        this.saldavel = saldavel;
+        this.doador = doador;
+    }
+
+    
+
+    
+
+
+
+}
