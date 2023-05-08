30566929197c3a6c6b600112b9c596bfec00c3cc
Lonalt <luann.alves353@gmail.com>
2023-05-08 17:45:05 -0300

    modified:   HSS/src/Teste/base.java

--- HSS/src/Teste/base.java
+++ HSS/src/Teste/base.java
@@ -5,10 +5,14 @@ import Dados.*;
 
 public class base {
     public static void main(String[] args) {
-        // Teste de Pessoa
-        Pessoa p = new Pessoa("João", "12345678910", "luann@gmail.com", LocalDate.of(1999, 5, 9), "M");
-        System.out.println("Nome: " + p.getNome());
-        //idade
-        System.out.println("Idade: " + p.getIdade());
+        // Teste de Doador (String nome, String cpf, String email, LocalDate dataNascimento, String sexo, String tipoSanguineo, String fatorRH, String endereco)
+        Doador d = new Doador("Lucas", "12345678910", "luacas@gmail", LocalDate.of(2000, 10, 10), "Masculino", "A", "+", "Rua 1");
+        System.out.println(d.getNome());
+
+        // Teste de AuxiliarCadastro (String nome, String cpf, String email, LocalDate dataNascimento, String sexo)
+        AuxiliarCadastro ac = new AuxiliarCadastro("Ana", "12515156110", "ana@gmail", LocalDate.of(1990, 05, 10), "Femino");
+        System.out.println(ac.isDisponivel());
+        
+        
     }
 }
