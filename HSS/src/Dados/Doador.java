6827caa44eef8428aa557e9c526896e9aa441646
Lonalt <luann.alves353@gmail.com>
2023-05-08 16:36:38 -0300

    new file:   HSS/src/Dados/Doador.java

---
+++ HSS/src/Dados/Doador.java
@@ -0,0 +1,35 @@
+package Dados;
+import java.time.LocalDate;
+
+public class Doador extends Pessoa{
+    private String tipoSanguineo;
+    private String fatorRH;
+    private String endereco;
+    private boolean cadastrado;
+
+    public Doador(String nome, String cpf, String email, LocalDate dataNascimento, String sexo, String tipoSanguineo, String fatorRH, String endereco) {
+        super(nome, cpf, email, dataNascimento, sexo);
+        this.cadastrado = false;
+        this.tipoSanguineo = tipoSanguineo;
+        this.fatorRH = fatorRH;
+        this.endereco = endereco;
+    }
+    public String getTipoSanguineo() {
+        return tipoSanguineo;
+    }
+    public String getFatorRH() {
+        return fatorRH;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public boolean isCadastrado() {
+        return cadastrado;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public void setCadastrado(boolean cadastrado) {
+        this.cadastrado = cadastrado;
+    }
+}
